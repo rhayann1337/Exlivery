d4fcba29b8d5180ba80de0bdb7d11bb20cb0ff0a
rhayann1337 <rhayannbianco@gmail.com>
2022-10-16 01:44:08 -0300

    creating Agents

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,4 +1,13 @@
 defmodule Exlivery do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
   def hello do
     :world
   end
--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Exlivery.MixProject do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:decimal, "~> 2.0"},
-      {:ex_machina, "~> 2.7.0"}
+      {:ex_machina, "~> 2.7.0"},
+      {:elixir_uuid, "~> 1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
